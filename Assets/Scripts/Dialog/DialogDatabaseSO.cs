114523ba9eb4c6607e618cdac1d4af54030b16b1
junhae0 <jh040101@g.shingu.ac.kr>
2026-03-25 12:49:40 +0900

    [Feat] Dialog Asset 데이터 정리

---
+++ Assets/Scripts/Dialog/DialogDatabaseSO.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "DialogDatabaseSO", menuName = "Dialog System/DialogDatabaseSO")]
+public class DialogDatabaseSO : ScriptableObject
+{
+    public List<DialogSO> dialogs = new List<DialogSO>();
+
+    private Dictionary<int, DialogSO> dialogsByld;                  //Ä³½ÌÀ» À§ÇÑ µñ¼Å³Ê¸® »ç¿ë
+
+    public void Initailize()
+    {
+        dialogsByld = new Dictionary<int, DialogSO>();
+
+        foreach (var dialog in dialogs)
+        {
+            if (dialog != null)
+            {
+                dialogsByld[dialog.id] = dialog;
+
+            }
+        }
+    }
+
+    public DialogSO GetDialongByld(int id)
+    {
+        if (dialogsByld == null)
+            Initailize();
+
+        if (dialogsByld.TryGetValue(id, out DialogSO dialog))
+        {
+            return dialog;
+        }
+
+        return null;
+    }
+}
